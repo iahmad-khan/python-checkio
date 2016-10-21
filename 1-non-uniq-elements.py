994b69fbc09cce097f84eae0f974d4bfcb0777c4
Ijaz ahmad khan <ijazahmad722@gmail.com>
2016-10-21 14:12:35 +0200

    Create 1-non-uniq-elements.py

---
+++ 1-non-uniq-elements.py
@@ -0,0 +1,23 @@
+#!/bin/python27
+
+def checkio(data):
+    
+    temp = []
+    for i in data:
+        if data.count(i) == 1:
+            temp.append(i)
+            
+    for i in temp:
+        data.remove(i)
+
+    #replace this for solution
+    return data
+
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
